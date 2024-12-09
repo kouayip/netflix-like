b381d81e664ad724ff1f4b02bda8509aea9fe527
Yves Kams <kamwayves37@gmail.com>
2024-12-09 12:14:36 +0100

    test: enable metadata

--- lib/seo.ts
+++ lib/seo.ts
@@ -3,12 +3,13 @@ import { Robots } from "next/dist/lib/metadata/types/metadata-types";
 
 const APP_TITLE = "Netflix Like";
 const APP_DESCRIPTION = `${APP_TITLE} is a application for film fanatics`;
-// const SITE_URL = process.env.SITE_URL || "http://localhost:3000";
+const SITE_URL = process.env.SITE_URL || "http://localhost:3000";
 
 export function getMetadata({
   title,
   description,
   pathname,
+  images,
   robots,
 }: {
   title?: string;
@@ -19,19 +20,19 @@ export function getMetadata({
 }): Metadata {
   const metaTitle = title ? `${title} | ${APP_TITLE}` : APP_TITLE;
   const metaDescription = description ?? APP_DESCRIPTION;
-  //   const metaImages = images ?? [
-  //     {
-  //       url: `${SITE_URL}/opengraph-image.jpg`,
-  //       width: 1024,
-  //       height: 580,
-  //       alt: `${APP_TITLE} Image Alt`,
-  //     },
-  //   ];
+  const metaImages = images ?? [
+    {
+      url: `${SITE_URL}/opengraph-image.jpg`,
+      width: 1024,
+      height: 580,
+      alt: `${APP_TITLE} Image Alt`,
+    },
+  ];
 
   return {
     title: metaTitle,
     description: metaDescription,
-    metadataBase: new URL(process.env.BASE_URL as string),
+    metadataBase: new URL(SITE_URL as string),
     creator: "Yves kams",
     applicationName: APP_TITLE,
     alternates: {
@@ -44,12 +45,14 @@ export function getMetadata({
       locale: "en_US",
       url: pathname,
       siteName: APP_TITLE,
+      images: metaImages,
     },
     twitter: {
       title: metaTitle,
       description: metaDescription,
       card: "summary_large_image",
       creator: "@yveskams_",
+      images: metaImages,
     },
     robots,
   };
