9a47050171c6d11e5626c80dcd8f9cbc40606e80
Yves Kams <kamwayves37@gmail.com>
2024-12-09 14:54:09 +0100

    refactor: remove unuse hook

--- hooks/useMovieDetails.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-
-export const useMovieDetails = (id: string | string[] | undefined) =>
-  useQuery({
-    queryKey: ['movie', id],
-    // queryFn: () => fetchMovieDetails(id),
-    enabled: !!id,
-  });
--- hooks/useSimilarMovies.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-
-export const useSimilarMovies = (category: string | undefined) =>
-  useQuery({
-    queryKey: ['similarMovies', category],
-    // queryFn: () => fetchSimilarMovies(category!),
-    enabled: !!category,
-  });
