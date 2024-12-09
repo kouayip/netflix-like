a062d291f28a5b51200780ac0a7c0d8313ad4685
Yves Kams <kamwayves37@gmail.com>
2024-12-09 11:59:53 +0100

    feat: add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <Container>
+      <h4>not-found</h4>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
